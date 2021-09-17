ea4201a9f3fb3f65fd0ff242d72d4a4446e68d94
ameya mellacheruvu <amellach@lion.lmu.edu>
2021-09-16 22:15:05 -0700

    getting started on beaker app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+  <Button variant="contained">Hello World</Button>
+  );
+}
+
+export default App;
