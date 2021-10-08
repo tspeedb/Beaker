27f2714ea7e578fbfd3466b3eadc15f9b641298b
mscott44 <moriahscott27@gmail.com>
2021-10-08 01:44:48 -0700

    welcome page changes

--- src/Pages/Welcomepage.js
+++ src/Pages/Welcomepage.js
@@ -8,45 +8,75 @@ import Box from '@mui/material/Box'
 function Welcomepage() {
     return (
         <div className="welcomepage-container">
+            {/* <video src="/videos/welcomevideo.mov" autoPlay loop muted /> */}
+            <div className="wrapper"></div>
             <div className="top-welcomepage">
-                {/* <video src="/videos/Dust-impact-03.mov" autoPlay loop muted /> */}
-                <img src={beakerLogo} alt="logo" />\
+                <img src={beakerLogo} alt="logo" />
+
                 <h1> LMU's very own hub for research and collaboration </h1>
             </div>
-            <p>
-                {' '}
-                Scientific beakers can be used to combine entities to produce a
-                solution. Similar to that, LMU's Beaker will bring together
-                people from different departments to produce a succesfull
-                research project
-            </p>
-            <div>
-                <Box
-                    sx={{
-                        display: 'grid',
-                        gridTemplateColumns: 'repeat(3, 1fr)',
+            <div style={{ backgroundColor: 'black', opacity: '85%' }}>
+                <p
+                    style={{
+                        textAlign: 'center',
+                        color: 'white',
                     }}
                 >
-                    <p>
-                        Find amazing talent <br></br>
-                        Post projects and see who matches your project
-                        qualifications
-                    </p>
-                    <p>
-                        {' '}
-                        Make yourself known <br></br>
-                        Get seen by those who are looking for someone just like
-                        you
-                    </p>
-                    <p>
-                        {' '}
-                        Make connections <br></br>
-                        Get connected with people outside of your major
-                    </p>
-                </Box>
-                <div className="button1">
+                    {' '}
+                    " Scientific beakers can be used to combine entities to
+                    produce a solution. Similar to that, LMU's Beaker <br></br>{' '}
+                    will bring together people from different departments to
+                    produce <br></br>a succesfull research project " - Beaker
+                    Team
+                </p>
+
+                <hr></hr>
+
+                <div>
+                    <Box
+                        sx={{
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(3, 1fr)',
+                        }}
+                    >
+                        <p
+                            style={{
+                                color: 'white',
+                            }}
+                        >
+                            <strong>Find amazing talent</strong> <br></br>
+                            Post projects and see who <br></br> matches your
+                            project qualifications
+                        </p>
+                        <p
+                            style={{
+                                color: 'white',
+                            }}
+                        >
+                            {' '}
+                            <strong> Make yourself known </strong> <br></br>
+                            Get seen by those who are <br></br> looking for
+                            someone just like you
+                        </p>
+                        <p
+                            style={{
+                                color: 'white',
+                            }}
+                        >
+                            {' '}
+                            <strong> Make connections </strong> <br></br>
+                            Get connected with people <br></br> outside of your
+                            major
+                        </p>
+                    </Box>
+                </div>
+                <div className="getstarted">
                     <Link className="button-link" to="/homepage">
-                        <Button className="btn1" size="large" color="primary">
+                        <Button
+                            className="getstarted-btn1"
+                            size="large"
+                            color="primary"
+                        >
                             Get started
                         </Button>
                     </Link>
