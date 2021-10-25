ec2b098eae7976e6ed59537e587d8044d2b36354
Ameya <amellach@lion.lmu.edu>
2021-10-24 19:57:55 -0700

    adding backend components, not working thou

--- src/Pages/Projectspage.js
+++ src/Pages/Projectspage.js
@@ -5,6 +5,9 @@ import { Typography } from '@material-ui/core'
 import projects from '../projectsdata'
 import { makeStyles } from '@material-ui/styles'
 import { Link } from 'react-router-dom'
+import { collection, query, where, getDocs, doc } from 'firebase/firestore'
+import firebase from 'firebase/compat/app'
+import 'firebase/firestore'
 
 const useStyles = makeStyles({
     title: { textAlign: 'left' },
@@ -13,12 +16,39 @@ const useStyles = makeStyles({
 
 function Projectspage() {
     const classes = useStyles()
+    // const [projects, setProjects] = useState([])
     const [browse, setBrowse] = useState([])
     const [profiles, setProfiles] = useState([])
     const [bookmarks, setBookmarks] = useState([])
 
+    // const projectCollection = firestore().collection('projects')
+    // console.log('Project:' + projectCollection)
+
+    // const fetchprojects = async (firebase) => {
+    //     const projectsRef = firebase.collection('projects')
+    //     const snapshot = await projectsRef.get()
+    //     snapshot.forEach((doc) => {
+    //         console.log(doc.id, '=>', doc.data())
+
+    // const q = query(
+    //     collection('projects'),
+    //     where('projects', '==', true)
+    // )
+
+    // const querySnapshot = await getDocs(q)
+    // querySnapshot.forEach((doc) => {
+    //     // doc.data() is never undefined for query doc snapshots
+    //         //     console.log(doc.id, ' => ', doc.data())
+    //     })
+    // }
+    // const response = firebase.collection('projects')
+    // const data = await response.get()
+    // data.docs.forEach((item) => {
+    //     setProjects([...projects, item.data()])
+
     useEffect(() => {
         //fetch data from server
+        // fetchprojects();
         const projects1 = projects.filter(
             (project) => project.type === 'browsed'
         )
@@ -33,6 +63,14 @@ function Projectspage() {
         }, 500)
     }, [])
 
+    // useEffect(() => {
+    //     fetchprojects()
+    // }, [])
+
+    // return (
+    //     <div>
+    //         {projects &&
+    //             projects.map((project) => {
     return (
         <Layout>
             <div>
@@ -64,6 +102,9 @@ function Projectspage() {
             </div>
         </Layout>
     )
+    // })}
+    //         </div>
+    //     )
 }
 
 export default Projectspage
