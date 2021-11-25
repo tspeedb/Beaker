2182d47ac9825a21632574f664796c53c1d68c63
mscott44 <moriahscott27@gmail.com>
2021-11-24 22:26:34 -0800

    fixed problem

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 // import React, { useState } from 'react'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -8,6 +8,11 @@ import IconButton from '@material-ui/core/IconButton'
 import { Button, Menu } from '@material-ui/core'
 import HelpIcon from '@mui/icons-material/Help'
 import { Link } from 'react-router-dom'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import Grow from '@material-ui/core/Grow'
+import Paper from '@material-ui/core/Paper'
+import Popper from '@material-ui/core/Popper'
+import MenuItem from '@material-ui/core/MenuItem'
 import '../Styles/Navbar.css'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
@@ -36,11 +41,44 @@ const useStyles = makeStyles((theme) => ({
         color: 'white',
         textDecoration: 'none',
     },
+    paper: {
+        marginRight: theme.spacing(2),
+    },
 }))
 
 const Navbar = () => {
     const classes = useStyles()
     const [item, setItem] = useState(null)
+    const [open, setOpen] = useState(false)
+    const anchorRef = useRef(null)
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen)
+    }
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return
+        }
+
+        setOpen(false)
+    }
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault()
+            setOpen(false)
+        }
+    }
+
+    const prevOpen = useRef(open)
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus()
+        }
+
+        prevOpen.current = open
+    }, [open])
 
     const handleMenuOpen = (event) => {
         setItem(event.currentTarget)
@@ -72,7 +110,13 @@ const Navbar = () => {
                         <HelpIcon />
                     </IconButton>
                     <Typography className={classes.space}></Typography>
-                    <div style={{ justifyContent: 'end', position: 'flex' }}>
+                    <div
+                        style={{
+                            justifyContent: 'end',
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
                         <Button className={classes.actionButtons}>
                             {' '}
                             <Link className={classes.links} to="/allmembers">
@@ -93,13 +137,72 @@ const Navbar = () => {
                             </Link>
                         </Button>
 
-                        <Button
+                        {/* <Button
                             aria-controls="menu"
                             onMouseOver={handleMenuOpen}
                             className={classes.actionButtons}
-                        >
-                            <AccountCircleIcon />
-                        </Button>
+                        ></Button> */}
+
+                        <div>
+                            <Button
+                                ref={anchorRef}
+                                aria-controls={
+                                    open ? 'menu-list-grow' : undefined
+                                }
+                                aria-haspopup="true"
+                                onClick={handleToggle}
+                                style={{ color: 'white' }}
+                            >
+                                <AccountCircleIcon />
+                            </Button>
+                            <Popper
+                                open={open}
+                                anchorEl={anchorRef.current}
+                                role={undefined}
+                                transition
+                                disablePortal
+                            >
+                                {({ TransitionProps, placement }) => (
+                                    <Grow
+                                        {...TransitionProps}
+                                        style={{
+                                            transformOrigin:
+                                                placement === 'bottom'
+                                                    ? 'center top'
+                                                    : 'center bottom',
+                                        }}
+                                    >
+                                        <Paper>
+                                            <ClickAwayListener
+                                                onClickAway={handleClose}
+                                            >
+                                                <MenuList
+                                                    autoFocusItem={open}
+                                                    id="menu-list-grow"
+                                                    onKeyDown={
+                                                        handleListKeyDown
+                                                    }
+                                                >
+                                                    <MenuItem
+                                                        onClick={handleClose}
+                                                    >
+                                                        Profile
+                                                    </MenuItem>
+                                                    <MenuItem
+                                                        onClick={handleClose}
+                                                    >
+                                                        <Link to="/">
+                                                            {' '}
+                                                            Logout{' '}
+                                                        </Link>
+                                                    </MenuItem>
+                                                </MenuList>
+                                            </ClickAwayListener>
+                                        </Paper>
+                                    </Grow>
+                                )}
+                            </Popper>
+                        </div>
                     </div>
                 </Toolbar>
             </AppBar>
