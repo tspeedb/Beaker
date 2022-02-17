91746cf861b7cededa5c1ce5941dde4bf04a8127
mscott44 <moriahscott27@gmail.com>
2022-02-17 12:01:35 -0800

    working on the table

--- src/Components/CardItems.js
+++ src/Components/CardItems.js
@@ -7,16 +7,51 @@ import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 // import { makeStyles } from '@material-ui/styles'
 import '../Styles/Projectspage.css'
 
 /*passing in properties from Cards*/
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        maxHeight: 200,
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    cover: {
+        width: 151,
+        paddingTop: 0,
+    },
+}))
+
 function CardItems({ project }) {
+    const classes = useStyles()
+    const theme = useTheme()
     return (
-        <Card sx={{ maxWidth: 300 }}>
+        <Card className={classes.root}>
+            <div className={classes.details}>
+                <CardContent className={classes.content}>
+                    <Typography id="title-project">{project.title}</Typography>
+                    {project.isPaid && (
+                        <MonetizationOnIcon
+                            style={{ color: '#107fb7' }}
+                        ></MonetizationOnIcon>
+                    )}
+                    {project.year && (
+                        <KeyboardArrowUpIcon></KeyboardArrowUpIcon>
+                    )}
+                    <Typography id="desc">{project.description}</Typography>
+                </CardContent>
+            </div>
             <CardMedia
-                style={{ paddingTop: 0 }}
+                className={classes.cover}
                 component="img"
                 height="200"
                 image={`${process.env.PUBLIC_URL}/projectImages/${project.image}`}
@@ -24,16 +59,6 @@ function CardItems({ project }) {
             >
                 {/* {props.image} */}
             </CardMedia>
-            <CardContent className="content">
-                <Typography id="title-project">{project.title}</Typography>
-                {project.isPaid && (
-                    <MonetizationOnIcon
-                        style={{ color: '#107fb7' }}
-                    ></MonetizationOnIcon>
-                )}
-                {project.year && <KeyboardArrowUpIcon></KeyboardArrowUpIcon>}
-                <Typography id="desc">{project.description}</Typography>
-            </CardContent>
         </Card>
     )
 }
